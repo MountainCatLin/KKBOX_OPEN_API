814cfa6b0e0d86235327ec00e8d241256740ffb9
Jammy Lin <jammy.lin@pakingtek.com>
2020-09-20 09:09:26 +0800

    some fixing

--- app/src/main/java/com/example/kkbox_open_api/AppInfo.kt
+++ app/src/main/java/com/example/kkbox_open_api/AppInfo.kt
@@ -5,10 +5,10 @@ object AppInfo {
     const val CLIENT_ID = "6feb73c883d0a7c726e4a7ae30e015a6"
     const val CLIENT_SECRET = "eb9e91f7741955fe80bab35b978c2511"
     //For GAN model config
-    const val LOW_IMAGE_SIZE = 300
-    const val HIGH_IMAGE_SIZE = 1200
-    const val LOW_IMAGE_RESOLUTION_FILE = "300x300.jpg"
-    const val GAN_MODEL_FILE_NAME = "srgan300.tflite"
+    const val LOW_IMAGE_SIZE = 50
+    const val HIGH_IMAGE_SIZE = 200
+    const val LOW_IMAGE_RESOLUTION_FILE = "50x50.jpg"
+    const val GAN_MODEL_FILE_NAME = "srgan50.tflite"
     //For tabbed view
     const val HIGH_RESOLUTION = "1000x1000"
     const val LOW_RESOLUTION = "300x300"
